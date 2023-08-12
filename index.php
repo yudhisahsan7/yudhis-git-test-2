5f29374576b9babb7aff7baae3eb2351b3b1aafa
yudhisahsan7 <yudhisahsan@gmail.com>
2023-08-12 12:50:18 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,10 @@
 <body>
   <h1>Hello World</h1>
   <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit nobis accusantium deleniti illum nesciunt fugit architecto, placeat ullam expedita ipsa nam quos labore molestiae dignissimos officiis ut laudantium tempore. Amet.</p>
+  <ul>
+    <li>satu</li>
+    <li>dua</li>
+    <li>tiga</li>
+  </ul>
 </body>
 </html>
